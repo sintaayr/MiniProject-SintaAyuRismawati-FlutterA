806b8dc6471eb80205708650f3a671d4c769b388
sintaayr <21082010069@student.upnjatim.ac.id>
2023-11-03 09:27:38 +0700

    ralat api key

--- lib/Provider/health_care_provider.dart
+++ lib/Provider/health_care_provider.dart
@@ -1,43 +1,43 @@
-// import 'package:flutter/material.dart';
-// import '../Model/chat_message.dart';
-// import '../Model/open_ai.dart';
-// import '../service/answer.dart';
+import 'package:flutter/material.dart';
+import '../Model/chat_message.dart';
+import '../Model/open_ai.dart';
+import '../service/answer.dart';
 
-// class HealthCareProvider extends ChangeNotifier {
-//   String _question = "";
-//   String _resultAI = "";
-//   final TextEditingController _messageController = TextEditingController();
-//   final List<ChatMessage> _chatMessages = [];
-//   late GptData gptResponseData;
+class HealthCareProvider extends ChangeNotifier {
+  String _question = "";
+  String _resultAI = "";
+  final TextEditingController _messageController = TextEditingController();
+  final List<ChatMessage> _chatMessages = [];
+  late GptData gptResponseData;
 
-//   String get question => _question;
-//   String get resultAI => _resultAI;
-//   TextEditingController get messageController => _messageController;
-//   List<ChatMessage> get chatMessages => _chatMessages;
+  String get question => _question;
+  String get resultAI => _resultAI;
+  TextEditingController get messageController => _messageController;
+  List<ChatMessage> get chatMessages => _chatMessages;
 
-//   void addMessage(ChatMessage message) {
-//     _chatMessages.add(message);
-//     _messageController.clear();
-//     notifyListeners();
-//   }
+  void addMessage(ChatMessage message) {
+    _chatMessages.add(message);
+    _messageController.clear();
+    notifyListeners();
+  }
 
-//   Future getRecomendations() async {
-//     final result = await RecommendationService.getSmartphoneRecommendations(
-//         question: _question);
-//     gptResponseData = result;
-//     _resultAI = gptResponseData.choices[0].text;
-//     _chatMessages
-//         .add(ChatMessage(messageContent: _resultAI, messageType: 'receiver'));
-//     notifyListeners();
-//   }
+  Future getRecomendations() async {
+    final result = await RecommendationService.getSmartphoneRecommendations(
+        question: _question);
+    gptResponseData = result;
+    _resultAI = gptResponseData.choices[0].text;
+    _chatMessages
+        .add(ChatMessage(messageContent: _resultAI, messageType: 'receiver'));
+    notifyListeners();
+  }
 
-//   set question(String question) {
-//     _question = question;
-//     notifyListeners();
-//   }
+  set question(String question) {
+    _question = question;
+    notifyListeners();
+  }
 
-//   set resultAI(String resultAI) {
-//     _resultAI = resultAI;
-//     notifyListeners();
-//   }
-// }
+  set resultAI(String resultAI) {
+    _resultAI = resultAI;
+    notifyListeners();
+  }
+}
--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,9 +1,9 @@
-// import 'package:envied/envied.dart';
+import 'package:envied/envied.dart';
 
-// part 'env.g.dart';
+part 'env.g.dart';
 
-// @Envied(path: '.env')
-// abstract class Env {
-//   @EnviedField(varName: 'OPENAI_API_KEY4')
-//   static const String apiKey = _Env.apiKey;
-// }
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'OPENAI_API_KEY4')
+  static const String apiKey = _Env.apiKey;
+}
